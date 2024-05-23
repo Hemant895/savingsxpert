37ce76b71822283c36bf2b7d5afdfb7d7edcc5e2
Hemant895 <hemantkhedkar102034@gmail.com>
2024-05-23 18:32:41 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Toaster/>
-        {children}</body>
+        {children}
+        
+        </body>
     </html>
     </ClerkProvider>
   );
